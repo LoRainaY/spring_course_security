717b0b28feb9327919ef1a2c70115c72977de4b4
Your Name <Yana Zaitsava>
2022-01-12 11:46:30 +0300

    add check password from BD

--- src/main/java/com/zaitsava/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaitsava/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zaitsava.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,17 +8,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity //помечаем, что класс ответственен за security конфигурации
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("yana").password("yana").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource); //подключение к бд
+
+
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("yana").password("yana").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+//                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
     }
 
     @Override
--- target/spring_course_security/WEB-INF/view/view-for-all-employees.jsp
+++ target/spring_course_security/WEB-INF/view/view-for-all-employees.jsp
@@ -21,7 +21,7 @@ Only for HR staff
 <br><br>
 <security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance" onclick="window.location.href='manager_info'">
-nly for Managers
+Only for Managers
 </security:authorize>
 
 </body>
